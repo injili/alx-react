549a58a02d1994c3891360cd277b18936d77aa09
injili <nyarekigospel@gmail.com>
2024-05-11 02:16:54 +0300

    this is a basic webpack setup

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$('body').appeng('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>CLick here to get started</button>')
+$('body').append("<p id'count'></p>");
+$('body').append('Copyright - Holberton School')
