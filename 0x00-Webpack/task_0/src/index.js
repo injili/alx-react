3d40f8a0b9ff564be6469592188a4096fe0ea8ad
injili <nyarekigospel@gmail.com>
2024-05-11 01:19:52 +0300

    this is a basic webpack setup

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,6 @@
 import $ from "jquery";
 
-$( "<p>Holberton Dashboard</p> " ).append("body");
-$( "<p>Dashboard data for the students</p>" ).append("body");
-$( "<p>Copyright - Holberton School</p>" ).append("body");
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
