fe238a5a8d0a9d0fc473d2eec170e9f2fc278d39
injili <nyarekigospel@gmail.com>
2024-05-11 22:40:45 +0300

    saturdY NIGHT

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -4,4 +4,4 @@ $('body').appeng('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>CLick here to get started</button>')
 $('body').append("<p id'count'></p>");
-$('body').append('Copyright - Holberton School')
+$('body').append('<p>Copyright - Holberton School</p>')
