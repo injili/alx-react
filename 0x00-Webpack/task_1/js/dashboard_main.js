67dec19cd124f0a942f7487f2742d32e63392859
injili <nyarekigospel@gmail.com>
2024-05-12 12:32:59 +0300

    backticks again

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').appeng('<p>Holberton Dashboard</p>');
+$('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>CLick here to get started</button>')
 $('body').append("<p id='count'></p>");
