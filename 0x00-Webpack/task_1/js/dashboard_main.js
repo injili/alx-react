cb3b52f1ffe7e84dd8814eb700f4451d23a6a8e8
injili <nyarekigospel@gmail.com>
2024-05-12 10:37:09 +0300

    the js is done

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,7 +1,16 @@
 import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').appeng('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>CLick here to get started</button>')
-$('body').append("<p id'count'></p>");
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>')
+
+let count = 0;
+function updateCounter() {
+    count ++;
+    $('#count').html('${count} click on the button'); 
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
