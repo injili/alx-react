19163d5b81b50771aba7a42b698c42a4e4572c6f
injili <nyarekigospel@gmail.com>
2024-05-20 23:25:45 +0300

    this is the beninging

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import * as css from '';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>CLick here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let theCount = 0;
+function updateCounter() {
+    theCount++;
+    $('#count').html(`${theCount} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
