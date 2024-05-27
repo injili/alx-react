3c564e3306612bc9fb91c1b446c04cd2b99f3a59
injili <nyarekigospel@gmail.com>
2024-05-27 20:58:12 +0300

    the beginning of react props

---
+++ 0x02-react_props/task_3/dashboard/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <img src={logo} alt="This Is Holberton Dawg"/>
+        <h1>School dashboard</h1>
+      </div>
+      <hr />
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label>
+            Email:
+            <input type='email' name='email' />
+          </label>
+          <label>
+            Password:
+            <input type='text' name='password' />
+          </label>
+          <input type='button' name='ok' value='OK' />
+        </form>
+      </div>
+      <div className="App-footer">
+      <hr />
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
