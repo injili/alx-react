74917fe04f2ba5b455d1c42e43c959dbe1caec60
injili <nyarekigospel@gmail.com>
2024-05-30 17:02:06 +0300

    the thing is here

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -14,7 +14,7 @@ export default function Notifications() {
             </button>
             <p>Here is the list of notifications</p>
             <ul>
-                <NotificationItem type="default" value="New course available" />
+                <NotificationItem type="urgent" value="New course available" />
                 <NotificationItem type="urgent" value="New resume available" />
                 <NotificationItem type="urgent" html={getLatestNotification()} />
             </ul>
